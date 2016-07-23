679bfa4048de276d1e6ff68324e587b2d8b406e7
langston8182 <cyril.marchive@gmail.com>
2016-07-23 19:47:02 +0200

    cv

---
+++ app/scripts/controllers/informations.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cvApp.controller:InformationsCtrl
+ * @description
+ * # InformationsCtrl
+ * Controller of the cvApp
+ */
+angular.module('cvApp')
+  .controller('InformationsCtrl', function ($scope) {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    var currIndex = 0;
+    $scope.slides = [];
+    $scope.slides.push({
+    	image: 'images/arduino.jpg',
+    	id: currIndex++,
+    	text: '96%'
+    });
+    $scope.slides.push({
+    	image: 'images/iot.jpg',
+    	id: currIndex++,
+    	text: '94%'
+    });
+  });
